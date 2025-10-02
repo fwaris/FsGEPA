588e3dde04d276a9c5b8b20fc737bcdff76f9f57
Faisal Waris <FaisalWaris@live.com>
2025-10-01 23:44:48 -0400

    sample data

---
+++ scripts/download.fsx
@@ -0,0 +1,19 @@
+﻿open System
+open System.IO
+open System.Net.Http
+
+let home = Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)
+
+let (@@) (a:string) (b:string) = Path.Combine(a,b)
+
+let url = @"http://curtis.ml.cmu.edu/datasets/hotpot/hotpot_dev_distractor_v1.json"
+
+let save (url:string) (path:string) = async {
+    use cl = new HttpClient()
+    use! str = cl.GetStreamAsync(url) |> Async.AwaitTask
+    use outstr = File.Create path
+    do! str.CopyToAsync(outstr) |> Async.AwaitTask
+}
+
+save url (home @@ "Downloads" @@ "hotpot_dev_distractor_v1.json") |> Async.RunSynchronously
+
---
+++ scripts/packages.fsx
@@ -0,0 +1,12 @@
+﻿#r "nuget: FSharp.Data"
+open System
+open System.IO
+open System.Net.Http
+
+[<AutoOpen>]
+module Utls = 
+    let home = Environment.GetFolderPath(System.Environment.SpecialFolder.UserProfile)
+
+    let (@@) (a:string) (b:string) = Path.Combine(a,b)
+
+
